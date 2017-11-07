6069ae1752bb7bac74861351c8b0373451c9e51d
Rickard Torén <ricto938@student.liu.se>
2017-11-07 17:51:26 +0100

    Reworked feature_layer.

--- examples/feature_layers.cc
+++ examples/feature_layers.cc
@@ -1,63 +1,164 @@
+void Initialize(const char* title, int x, int y, int w, int h, unsigned int flags = 0);
+void Shutdown();
+void Matrix1BPP(const char* bytes, int w_mat, int h_mat, int off_x, int off_y, int px_w, int px_h);
+void Matrix8BPPHeightMap(const char* bytes, int w_mat, int h_mat, int off_x, int off_y, int px_w, int px_h);
+void Matrix8BPPPlayers(const char* bytes, int w_mat, int h_mat, int off_x, int off_y, int px_w, int px_h);
+void ImageRGB(const char* bytes, int width, int height, int off_x, int off_y);
+void Render();
+
+#include "SDL.h"
+
+#include <cassert>
+#include <iostream>
+
+namespace {
+    SDL_Window* window_;
+    SDL_Renderer* renderer_;
+
+    SDL_Rect CreateRect(int x, int y, int w, int h) {
+        SDL_Rect r;
+        r.x = x;
+        r.y = y;
+        r.w = w;
+        r.h = h;
+        return r;
+    }
+}
+
+void Initialize(const char* title, int x, int y, int w, int h, unsigned int flags) {
+    int init_result = SDL_Init(SDL_INIT_VIDEO);
+    assert(!init_result);
+
+    window_ = SDL_CreateWindow(title, x, y, w, h, flags == 0 ? SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE : flags);
+    assert(window_);
+
+    renderer_ = SDL_CreateRenderer(window_, -1, SDL_RENDERER_ACCELERATED);
+    assert(renderer_);
+
+    // Clear window to black.
+    SDL_SetRenderDrawColor(renderer_, 0, 0, 0, 255);
+    SDL_RenderClear(renderer_);
+    SDL_SetRenderDrawBlendMode(renderer_, SDL_BLENDMODE_BLEND);
+}
+
+void Shutdown() {
+    SDL_DestroyRenderer(renderer_);
+    SDL_DestroyWindow(window_);
+    SDL_Quit();
+}
+
+void Matrix8BPPHeightMap(const char* bytes, int w_mat, int h_mat, int off_x, int off_y, int px_w, int px_h) {
+    assert(renderer_);
+    assert(window_);
+
+    SDL_Rect rect = CreateRect(0, 0, px_w, px_h);
+    for (size_t y = 0; y < h_mat; ++y) {
+        for (size_t x = 0; x < w_mat; ++x) {
+            rect.x = off_x + (int(x) * rect.w);
+            rect.y = off_y + (int(h_mat - 1 - y) * rect.h);
+
+            // Renders the height map in grayscale [0-255]
+            size_t index = x + y * w_mat;
+            SDL_SetRenderDrawColor(renderer_, bytes[index], bytes[index], bytes[index], 255);
+            SDL_RenderFillRect(renderer_, &rect);
+        }
+    }
+}
+
+void ImageRGB(const char* bytes, int width, int height, int off_x, int off_y) {
+    assert(renderer_);
+    assert(window_);
+
+    SDL_Surface* surface = SDL_CreateRGBSurfaceFrom((void*)bytes, width, height, 24, 3 * width, 0xFF0000, 0x00FF00, 0x0000FF, 0x000000);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer_, surface);
+    SDL_FreeSurface(surface);
+
+    SDL_Rect dstRect = CreateRect(off_x, off_y, width, height);
+    SDL_RenderCopy(renderer_, texture, NULL, &dstRect);
+
+    SDL_DestroyTexture(texture);
+}
+
+void Render() {
+    assert(renderer_);
+    assert(window_);
+
+    // Clear window to black.
+    SDL_SetRenderDrawColor(renderer_, 0, 0, 0, 255);
+
+    SDL_RenderPresent(renderer_);
+    SDL_RenderClear(renderer_);
+}
+
 #include "tutorial_bot.cc"
 
 #include "sc2api/sc2_api.h"
 #include "sc2utils/sc2_manage_process.h"
 
-#include <iostream>
-
-#include "sc2renderer/sc2_renderer.h"
-
-const float kCameraWidth = 44.0f; // default 24.0f
-const int kFeatureLayerSize = 80;
-const int kPixelDrawSize = 4;
-const int kDrawSize = kFeatureLayerSize * kPixelDrawSize;
-
-void DrawFeatureLayer1BPP(const SC2APIProtocol::ImageData& image_data, int off_x, int off_y) {
-    assert(image_data.bits_per_pixel() == 1);
-    int width = image_data.size().x();
-    int height = image_data.size().y();
-    sc2::renderer::Matrix1BPP(image_data.data().c_str(), width, height, off_x, off_y, kPixelDrawSize, kPixelDrawSize);
-}
-
-void DrawFeatureLayerUnits8BPP(const SC2APIProtocol::ImageData& image_data, int off_x, int off_y) {
-    assert(image_data.bits_per_pixel() == 8);
-    int width = image_data.size().x();
-    int height = image_data.size().y();
-    sc2::renderer::Matrix8BPPPlayers(image_data.data().c_str(), width, height, off_x, off_y, kPixelDrawSize, kPixelDrawSize);
-}
-
-void DrawFeatureLayerHeightMap8BPP(const SC2APIProtocol::ImageData& image_data, int off_x, int off_y) {
-    assert(image_data.bits_per_pixel() == 8);
-    int width = image_data.size().x();
-    int height = image_data.size().y();
-    sc2::renderer::Matrix8BPPHeightMap(image_data.data().c_str(), width, height, off_x, off_y, kPixelDrawSize, kPixelDrawSize);
-}
+//const float kCameraWidth = 44.0f; // default 24.0f
+//const int kFeatureLayerSize = 80;
+//const int kPixelDrawSize = 4;
+//const int kDrawSize = kFeatureLayerSize * kPixelDrawSize;
+const int sw = 720;
+const int sh = 720;
 
 class RenderAgent : public Bot {
 public:
     virtual void OnGameStart() final {
         Bot::OnGameStart();
-        sc2::renderer::Initialize("Feature layers", 350, 50, 2 * kDrawSize, 2 * kDrawSize);
+        Initialize("StarCraft 1.5", 550, 0, sw, sh);
     }
 
     virtual void OnStep() final {
         Bot::OnStep();
-        const SC2APIProtocol::Observation* observation = Observation()->GetRawObservation();
 
-        const SC2APIProtocol::FeatureLayers& m = observation->feature_layer_data().renders();
-        DrawFeatureLayerUnits8BPP(m.unit_density(), 0, 0);
-        DrawFeatureLayer1BPP(m.selected(), kDrawSize, 0);
+        // Set scale used when render, easier but also less details.
+        const GameInfo& GameInfo = Observation()->GetGameInfo();
+        float scale_x = sw / (float) GameInfo.width;
+        float scale_y = sh / (float) GameInfo.height;
+        SDL_RenderSetScale(renderer_, scale_x, scale_y);
 
-        const SC2APIProtocol::FeatureLayersMinimap& mi = observation->feature_layer_data().minimap_renders();
-        DrawFeatureLayerHeightMap8BPP(mi.height_map(), 0, kDrawSize);
-        DrawFeatureLayer1BPP(mi.camera(), kDrawSize, kDrawSize);
+        // Draw height map.
+        auto height_map = Observation()->GetRawObservation()->feature_layer_data().minimap_renders().height_map();
+        assert(height_map.bits_per_pixel() == 8);
+        int hm_width = height_map.size().x();
+        int hm_height = height_map.size().y();
+        Matrix8BPPHeightMap(height_map.data().c_str(), hm_width, hm_height, 0, 0, 2, 2);
 
-        sc2::renderer::Render();
+        // Draw fog of war.
+//        auto units = Observation()->GetUnits(sc2::Unit::Alliance::Self);
+//        SDL_SetRenderDrawColor(renderer_, 10, 10, 50, 255);
+//        for (auto unit : units) {
+//            int x = unit->pos.x;
+//            int y = unit->pos.y;
+//            int r = 4;
+//            int d = 8;
+//            SDL_Rect rect = CreateRect(x - r, y - r, d, d);
+//            SDL_RenderFillRect(renderer_, &rect);
+//        }
+
+        // Draw all units.
+        auto units = Observation()->GetUnits();
+        for (auto unit : units) {
+            int red[] = {123, 20, 50, 25, 250};
+            int green[] = {123, 50, 100, 250, 25};
+            int blue[] = {123, 250, 200, 25, 15};
+            int al = unit->alliance;
+            SDL_SetRenderDrawColor(renderer_, red[al], green[al], blue[al], 180);
+            int x = unit->pos.x;
+            int y = unit->pos.y;
+            int r = unit->radius;
+            int d = r <= 0 ? 1 : r * 2;
+            SDL_Rect rect = CreateRect(x - r, y - r, d, d);
+            SDL_RenderFillRect(renderer_, &rect);
+        }
+
+        Render();
     }
 
     virtual void OnGameEnd() final {
         Bot::OnGameEnd();
-        sc2::renderer::Shutdown();
+        Shutdown();
     }
 };
 
@@ -67,6 +168,8 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    const float kCameraWidth = 24.0f;
+    const int kFeatureLayerSize = 80;
     sc2::FeatureLayerSettings settings(kCameraWidth, kFeatureLayerSize, kFeatureLayerSize, kFeatureLayerSize, kFeatureLayerSize);
     coordinator.SetFeatureLayers(settings);
 
@@ -82,7 +185,27 @@ int main(int argc, char* argv[]) {
     coordinator.LaunchStarcraft();
     coordinator.StartGame(sc2::kMapBelShirVestigeLE);
 
+    SDL_Event event;
     while (coordinator.Update()) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT: {
+                    Shutdown();
+                    return 0;
+                }
+                case SDL_KEYDOWN: {
+                    std::cout << "KEYDOWN " << event.key.keysym.sym << std::endl;
+                    break;
+                }
+                case SDL_KEYUP: {
+                    std::cout << "KEYUP " << event.key.keysym.sym << std::endl;
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+        }
         if (sc2::PollKeyPress()) {
             break;
         }
