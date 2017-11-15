37c14f3af6e89c9057f07351627a1c997f440b96
Pjoelj <pjoelj1@gmail.com>
2017-11-15 16:12:12 +0100

    Setting up unit ID -> unit data map implemented

--- examples/kurt/build_manager.cc
+++ examples/kurt/build_manager.cc
@@ -1,5 +1,6 @@
 #include <sc2api/sc2_api.h>
 #include <iostream>
+#include <map>
 
 using namespace sc2;
 
@@ -10,6 +11,18 @@ public:
     }
     void OnStep (const ObservationInterface* observation) {
         // DO ALL DE ARMY STUFF
+    }
+    void OnGameStart(const ObservationInterface* observation) {
+        // Set up build tree
+        SetUpUnitDataMap(observation);
+    }
+
+    std::map<UnitTypeID, UnitTypeData> unit_types;
+private:
+    void SetUpUnitDataMap(const ObservationInterface *observation) {
+        for (UnitTypeData id : observation->GetUnitTypeData()) {
+            unit_types[id.unit_type_id] = id;
+        }
     }
 };
 
