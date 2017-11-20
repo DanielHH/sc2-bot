7a32d0e8860996380248500ab2651cf414db7596
Rickard Torén <ricto938@student.liu.se>
2017-11-20 21:35:02 +0100

    BPPlan: AddBasicPlan is good now :)

--- examples/kurt/BPPlan.cc
+++ examples/kurt/BPPlan.cc
@@ -9,15 +9,15 @@
 
 #include <iostream>
 #include <queue>
-#include <set>
+#include <stack>
 
 using namespace sc2;
 
 void BPPlan::AddBasicPlan(BPState * const start,
         BPState * const goal) {
     BPState built(start);
-    std::queue<BPAction> add_to_plan;
-    std::set<UNIT_TYPEID> need_to_build;
+    std::stack<BPAction> add_to_plan;
+    std::queue<UNIT_TYPEID> need_to_build;
     int mineral_cost = 0;
     int vespene_cost = 0;
     /*
@@ -29,7 +29,7 @@ void BPPlan::AddBasicPlan(BPState * const start,
         if (built.GetUnitAmount(type) >= amount) {
             continue;
         }
-        need_to_build.insert(type);
+        need_to_build.push(type);
         for (int i = start->GetUnitAmount(type); i < amount; ++i) {
             add_to_plan.push(BPAction::CreatesUnit(type));
         }
@@ -40,30 +40,40 @@ void BPPlan::AddBasicPlan(BPState * const start,
     /*
      * Add buildings required for to reach the goal.
      */
+    // TODO Chech if alias exist, should be barracks tech lab > barracks and NOT !=
     while (! need_to_build.empty()) {
-        UNIT_TYPEID curr = *(need_to_build.begin());
-        need_to_build.erase(need_to_build.begin());
-        // TODO Not use set for need_to_build ??
-        if (built.GetUnitAmount(curr) > 0) {
-            continue;
-        }
+        UNIT_TYPEID curr = need_to_build.front();
+        need_to_build.pop();
         for (UNIT_TYPEID req : BuildManager::GetRequirements(curr)) {
             if (built.GetUnitAmount(req) > 0) {
                 continue;
             }
-            need_to_build.insert(req);
+            need_to_build.push(req);
             add_to_plan.push(BPAction::CreatesUnit(req));
             built.SetUnitAmount(req, 1);
             mineral_cost += Kurt::GetUnitType(req)->mineral_cost;
             vespene_cost += Kurt::GetUnitType(req)->vespene_cost;
         }
     }
-    // TODO build vespene if needed ??
+    /*
+     * Add refinery if any building requires vespene gas.
+     *
+     * TODO Is this bad for MCTS?
+     */
+    if (vespene_cost > 0) {
+        UNIT_TYPEID refinery = UNIT_TYPEID::TERRAN_REFINERY;
+        built.SetUnitAmount(refinery, 1);
+        add_to_plan.push(BPAction(0, BPAction::GATHER_VESPENE));
+        add_to_plan.push(BPAction(0, BPAction::GATHER_VESPENE));
+        add_to_plan.push(BPAction::CreatesUnit(refinery));
+        mineral_cost += Kurt::GetUnitType(refinery)->mineral_cost;
+        vespene_cost += Kurt::GetUnitType(refinery)->vespene_cost;
+    }
     /*
      * Add all actions in reverse.
      */
     while (! add_to_plan.empty()) {
-        push_back(add_to_plan.front());
+        push_back(add_to_plan.top());
         add_to_plan.pop();
     }
 }
