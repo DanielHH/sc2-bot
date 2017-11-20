49c07a4dc0f7767f899d333fde579f368a7464a3
Rickard Torén <ricto938@student.liu.se>
2017-11-20 13:45:09 +0100

    Started with AddBasicPlan.

--- examples/kurt/BPPlan.cc
+++ examples/kurt/BPPlan.cc
@@ -1,7 +1,14 @@
 #include "BPPlan.h"
 #include "BPState.h"
 
+#include <queue>
+#include <set>
+
+using namespace sc2;
+
 void BPPlan::AddBasicPlan(BPState const * const start, BPState const * const goal) {
+    std::queue<BPAction *> toAdd;
+    std::set<UNIT_TYPEID> toBuild;
     // TODO
 }
 
