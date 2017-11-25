0c60aa7e89b1555f7259cff38ff8b7fefffe3a6a
Richard Almgren Friberg <rical803@student.liu.se>
2017-11-25 13:41:31 +0100

    populateworldRepresentation works as it should

--- examples/kurt/kurt.cc
+++ examples/kurt/kurt.cc
@@ -23,10 +23,19 @@ void Kurt::OnGameStart() {
     build_manager->OnGameStart(Observation());
     strategy_manager = new StrategyManager();
     PopulateWorldRepresentation();
-    std::cout << observation->GetGameInfo().pathing_grid.height << std::endl;
-    std::cout << "Height: " << observation->GetGameInfo().pathing_grid.width << std::endl;
-    std::cout << "world_rep Width " << (world_representation.front()).size() << std::endl;
+    std::cout << "Width: " << observation->GetGameInfo().pathing_grid.width << std::endl;
+    std::cout << "Height: " << observation->GetGameInfo().pathing_grid.height << std::endl;
+    std::cout << "world_rep Width " << (world_representation.back()).size() << std::endl;
     std::cout << "world_rep height " << world_representation.size() << std::endl;
+    
+    
+    for (int y = 0; y < world_representation.size(); y++) {
+        for (int x = 0; x < world_representation.at(y).size(); x++) {
+            std::cout << "Cell X: " << x << ", Y: " << y << std::endl;
+            std::cout << "Minerals: " << world_representation.at(y).at(x)->GetMineralAmount() << std::endl;
+            std::cout << "Gas: " << world_representation.at(y).at(x)->GetGasAmount() << std::endl;
+        }
+    }
     
 }
 
@@ -181,21 +190,60 @@ void Kurt::PopulateWorldRepresentation(){
     int rest_height = actual_world.height % chunk_size;
     int rest_width = actual_world.width % chunk_size;
     
+    // add cells
     for (int y = 1; y <= actual_world.height; ++y) {
         if (y % chunk_size == 0) {
             world_representation.push_back(std::vector<WorldCell*>());
-        } else if (y == actual_world.height-rest_height)
-        
+        } else if (y == actual_world.height && rest_height > 0) {
+            world_representation.push_back(std::vector<WorldCell*>());
+        }
         for (int x = 1; x <= actual_world.width; ++x) {
             if (x % chunk_size == 0 && y % chunk_size == 0) {
-                (world_representation.front()).push_back(new WorldCell(x,y));
-            } else if (x == actual_world.width - rest_width && y % chunk_size == 0) {
-                (world_representation.front()).push_back(new WorldCell(x+rest_width, y));
-            } else if (x % chunk_size == 0 && y == actual_world.height - rest_height) {
-                (world_representation.front()).push_back(new WorldCell(x, y+rest_height));
-            } else if (x == actual_world.width - rest_width && y == actual_world.height - rest_height) {
-                (world_representation.front()).push_back(new WorldCell(x+rest_width, y+rest_height));
+                (world_representation.back()).push_back(new WorldCell(x,y));
+            } else if (x == actual_world.width && rest_width > 0 && y % chunk_size == 0) {
+                (world_representation.back()).push_back(new WorldCell(x, y));
+            } else if (x % chunk_size == 0 && y == actual_world.height && rest_height > 0) {
+                (world_representation.back()).push_back(new WorldCell(x, y));
+            } else if (x == actual_world.width && rest_width > 0 && y == actual_world.height && rest_height > 0) {
+                (world_representation.back()).push_back(new WorldCell(x, y));
             }
+        }
+    }
+    
+    // put in all neutral units
+    for (const Unit* neutral_unit: Observation()->GetUnits(Unit::Alliance::Neutral)) {
+        int x_pos = neutral_unit->pos.x / chunk_size;
+        int y_pos  = neutral_unit->pos.y / chunk_size;
+        float mineral_amount = world_representation.at(y_pos).at(x_pos)->GetMineralAmount();
+        float gas_amount = world_representation.at(y_pos).at(x_pos)->GetGasAmount();
+        
+        /*
+        std::cout << "x: " << neutral_unit->pos.x << " y: " << neutral_unit->pos.y << std::endl;
+        std::cout << "X_pos: " << x_pos << " Y_pos: " << y_pos << std::endl;
+        */
+        
+        switch (neutral_unit->unit_type.ToType()) {
+            case UNIT_TYPEID::NEUTRAL_VESPENEGEYSER:
+                if (neutral_unit->vespene_contents > 0) {
+                    world_representation.at(y_pos).at(x_pos)->SetGasAmount(neutral_unit->vespene_contents + gas_amount);
+                } else { // cant see. maybe set to an approximation or inf ?
+                    world_representation.at(y_pos).at(x_pos)->SetGasAmount(2000 + gas_amount);
+                }
+                break;
+            case UNIT_TYPEID::NEUTRAL_MINERALFIELD:
+            case UNIT_TYPEID::NEUTRAL_MINERALFIELD750:
+            case UNIT_TYPEID::NEUTRAL_RICHMINERALFIELD:
+            case UNIT_TYPEID::NEUTRAL_RICHMINERALFIELD750:
+                if (neutral_unit->mineral_contents > 0) {
+                    world_representation.at(y_pos).at(x_pos)->SetMineralAmount(neutral_unit->mineral_contents + mineral_amount);
+                } else { // cant see. maybe set to an approximation or inf ?
+                    world_representation.at(y_pos).at(x_pos)->SetMineralAmount(1200 + mineral_amount);
+                }
+                break;
+            default:
+                // what todo with these?
+                std::cout << "Unknown type: " << neutral_unit->unit_type << std::endl;
+                break;
         }
     }
 }
