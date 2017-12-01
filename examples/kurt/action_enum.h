268e2fba1120bb802d07a39782cfb309ee8520f9
Rickard Torén <ricto938@student.liu.se>
2017-12-01 09:46:54 +0100

    action_enum: Added ActiveAction and < operator.

--- examples/kurt/action_enum.h
+++ examples/kurt/action_enum.h
@@ -50,3 +50,13 @@ enum class ACTION {
 
     // Other
 };
+
+class ActiveAction {
+public:
+    ACTION action;
+    double time_left;
+};
+
+bool operator<(ActiveAction const & lhs, ActiveAction const & rhs) {
+    return lhs.time_left < rhs.time_left;
+}
