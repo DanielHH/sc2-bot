ed2b72633ec4b21e554187ac77cb7125b3053810
Daniel Herzegh <danhe178@student.liu.se>
2017-12-01 18:52:49 +0100

    simple correction

--- examples/kurt/plans.h
+++ examples/kurt/plans.h
@@ -9,7 +9,7 @@ public:
 
     GamePlan* RushPlan(Kurt* kurt);
 
-    GamePlan* Plans::CruiserPlan(Kurt* kurt);
+    GamePlan* CruiserPlan(Kurt* kurt);
 
     GamePlan* DynamicGamePlan(Kurt* kurt);
 };
