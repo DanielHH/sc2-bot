c161e782f2afd967d80c1b411b4f00db6ba36cb5
Rickard Torén <ricto938@student.liu.se>
2017-12-05 20:32:14 +0100

    constants: Fixed so vespene and food_used no longer uses the same id.

--- examples/kurt/constants.h
+++ examples/kurt/constants.h
@@ -27,16 +27,16 @@ public:
     // Resources
     const static sc2::UNIT_TYPEID MINERALS = static_cast<sc2::UNIT_TYPEID>(524);
     const static sc2::UNIT_TYPEID VESPENE = static_cast<sc2::UNIT_TYPEID>(525);
-    const static sc2::UNIT_TYPEID FOOD_USED = static_cast<sc2::UNIT_TYPEID>(525);
-    const static sc2::UNIT_TYPEID FOOD_CAP = static_cast<sc2::UNIT_TYPEID>(526);
+    const static sc2::UNIT_TYPEID FOOD_USED = static_cast<sc2::UNIT_TYPEID>(526);
+    const static sc2::UNIT_TYPEID FOOD_CAP = static_cast<sc2::UNIT_TYPEID>(527);
 
     // Multi-representation units/structures
-    const static sc2::UNIT_TYPEID TERRAN_ANY_COMMANDCENTER = static_cast<sc2::UNIT_TYPEID>(527);
-    const static sc2::UNIT_TYPEID TERRAN_ANY_BARRACKS = static_cast<sc2::UNIT_TYPEID>(528);
-    const static sc2::UNIT_TYPEID TERRAN_ANY_FACTORY = static_cast<sc2::UNIT_TYPEID>(529);
-    const static sc2::UNIT_TYPEID TERRAN_ANY_STARPORT = static_cast<sc2::UNIT_TYPEID>(530);
+    const static sc2::UNIT_TYPEID TERRAN_ANY_COMMANDCENTER = static_cast<sc2::UNIT_TYPEID>(528);
+    const static sc2::UNIT_TYPEID TERRAN_ANY_BARRACKS = static_cast<sc2::UNIT_TYPEID>(529);
+    const static sc2::UNIT_TYPEID TERRAN_ANY_FACTORY = static_cast<sc2::UNIT_TYPEID>(530);
+    const static sc2::UNIT_TYPEID TERRAN_ANY_STARPORT = static_cast<sc2::UNIT_TYPEID>(531);
 
     // How many new scv that can be produced
-    const static sc2::UNIT_TYPEID TERRAN_TOWNHALL_SCV_MINERALS = static_cast<sc2::UNIT_TYPEID>(531);
-    const static sc2::UNIT_TYPEID TERRAN_TOWNHALL_SCV_VESPENE = static_cast<sc2::UNIT_TYPEID>(532);
+    const static sc2::UNIT_TYPEID TERRAN_TOWNHALL_SCV_MINERALS = static_cast<sc2::UNIT_TYPEID>(532);
+    const static sc2::UNIT_TYPEID TERRAN_TOWNHALL_SCV_VESPENE = static_cast<sc2::UNIT_TYPEID>(533);
 };
