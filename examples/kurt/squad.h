1b0845c9a0bca4dc7b15ad55111d5254ba042ee3
Richard Almgren Friberg <rical803@student.liu.se>
2017-12-07 10:08:37 +0100

    fixed so that the sqad files autocompletes

--- examples/kurt/squad.h
+++ examples/kurt/squad.h
@@ -1,21 +1,12 @@
 #pragma once
 
-#include "sc2api/sc2_api.h"
+#include <sc2api/sc2_api.h>
 
 class Squad {
-Private:
+private:
     sc2::Units members;
     void groupUp() {
     }
-Public:
-    void attackMove(sc2::Point2D point) {
-        bool squad_grouped = true;
-        for (const sc2::Unit* unit_a : members) {
-            for (const sc2::Unit*  unit_b: members) {
-                if (unit_a != unit_b) {
-                    // check distance
-                }
-            }
-        }
-    }
-}
+public:
+    void attackMove(sc2::Point2D point);
+};
