8f92e82ef058075a6a1648b5117d954e3e0c48d4
Richard Almgren Friberg <rical803@student.liu.se>
2017-12-07 10:59:06 +0100

    squad functions implemented

--- examples/kurt/squad.cc
+++ examples/kurt/squad.cc
@@ -1,12 +1,47 @@
 #include "squad.h"
 
+Squad::Squad(sc2::Units units, Kurt* parent_kurt) {
+    members = units;
+    kurt = parent_kurt;
+}
+
 void Squad::attackMove(sc2::Point2D point) {
+    if (isGrouped()) {
+        // attack
+        for (const sc2::Unit* unit: members) {
+            kurt->Actions()->UnitCommand(unit, sc2::ABILITY_ID::ATTACK, point);
+        }
+    } else {
+        groupUp();
+    }
+}
+
+bool Squad::isGrouped() {
     bool squad_grouped = true;
-    for (const sc2::Unit* unit_a : members) {
-        for (const sc2::Unit*  unit_b: members) {
-            if (unit_a != unit_b) {
+    if (!members.empty()) {
+        const sc2::Unit* unit_leader = members.front();
+        for (const sc2::Unit* unit_a : members) {
+            if (unit_a != unit_leader) {
                 // check distance
+                if (sc2::Distance2D(unit_a->pos, unit_leader->pos) > 4) {
+                    squad_grouped = false;
+                    break;
+                }
             }
         }
+    }
+    return squad_grouped;
+}
+
+void Squad::groupUp() {
+    float tot_x = 0;
+    float tot_y = 0;
+    for (const sc2::Unit* unit : members) {
+        tot_x += unit->pos.x;
+        tot_y += unit->pos.y;
+    }
+    sc2::Point2D meetup_point = sc2::Point2D(tot_x / members.size(), tot_y / members.size());
+    for (const sc2::Unit* unit : members) {
+        kurt->Actions()->UnitCommand(unit, sc2::ABILITY_ID::MOVE, meetup_point);
     }
 }
--- examples/kurt/squad.h
+++ examples/kurt/squad.h
@@ -1,12 +1,16 @@
 #pragma once
 
 #include <sc2api/sc2_api.h>
+#include "kurt.h"
 
 class Squad {
 private:
     sc2::Units members;
-    void groupUp() {
-    }
+    Kurt* kurt;
+    bool isGrouped();
+    void groupUp();
+    
 public:
+    Squad(sc2::Units units, Kurt* parent_kurt);
     void attackMove(sc2::Point2D point);
 };
